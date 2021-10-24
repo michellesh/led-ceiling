6b2ce7d1c7c842ab4dbf2deb28d25159c0f2ead3
Michelle Sharer <michellesharer@gmail.com>
2021-10-24 17:09:06 -0600

    Send both joystick values in same button struct

--- arduinoUno/arduinoUno.ino
+++ arduinoUno/arduinoUno.ino
@@ -5,7 +5,21 @@
 N64Controller n64(2);
 SoftwareSerial espSerial(5, 6);
 
-int lastValue[NUM_BUTTONS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+Button start = {N64_START, 0, 0};
+Button z = {N64_Z, 0, 0};
+Button b = {N64_B, 0, 0};
+Button a = {N64_A, 0, 0};
+Button l = {N64_L, 0, 0};
+Button r = {N64_R, 0, 0};
+Button cUp = {N64_CUP, 0, 0};
+Button cDown = {N64_CDOWN, 0, 0};
+Button cRight = {N64_CRIGHT, 0, 0};
+Button cLeft = {N64_CLEFT, 0, 0};
+Button dUp = {N64_DUP, 0, 0};
+Button dDown = {N64_DDOWN, 0, 0};
+Button dRight = {N64_DRIGHT, 0, 0};
+Button dLeft = {N64_DLEFT, 0, 0};
+Button joystick = {N64_JOYSTICK, 0, 0};
 
 void setup() {
   Serial.begin(115200);
@@ -15,36 +29,30 @@ void setup() {
 
 void loop() {
   n64.update();
-  for (int buttonId = 0; buttonId < NUM_BUTTONS; buttonId++) {
-    int buttonValue = getValue(buttonId);
-    if (buttonValue != lastValue[buttonId]) {
-      lastValue[buttonId] = buttonValue;
-      String str = stringifyMsg(buttonId, buttonValue);
-      Serial.println(str);
-      espSerial.println(str);
-    }
-  }
+  updateButton(start, n64.Start(), 0);
+  updateButton(z, n64.Z(), 0);
+  updateButton(b, n64.B(), 0);
+  updateButton(a, n64.A(), 0);
+  updateButton(l, n64.L(), 0);
+  updateButton(r, n64.R(), 0);
+  updateButton(cUp, n64.C_up(), 0);
+  updateButton(cDown, n64.C_down(), 0);
+  updateButton(cRight, n64.C_right(), 0);
+  updateButton(cLeft, n64.C_left(), 0);
+  updateButton(dUp, n64.D_up(), 0);
+  updateButton(dDown, n64.D_down(), 0);
+  updateButton(dRight, n64.D_right(), 0);
+  updateButton(dLeft, n64.D_left(), 0);
+  updateButton(joystick, n64.axis_x(), n64.axis_y());
   delay(10);
 }
 
-int getValue(int buttonId) {
-  switch (buttonId) {
-    case N64_START: return n64.Start();
-    case N64_Z: return n64.Z();
-    case N64_B: return n64.B();
-    case N64_A: return n64.A();
-    case N64_L: return n64.L();
-    case N64_R: return n64.R();
-    case N64_CUP: return n64.C_up();
-    case N64_CDOWN: return n64.C_down();
-    case N64_CRIGHT: return n64.C_right();
-    case N64_CLEFT: return n64.C_left();
-    case N64_DUP: return n64.D_up();
-    case N64_DDOWN: return n64.D_down();
-    case N64_DRIGHT: return n64.D_right();
-    case N64_DLEFT: return n64.D_left();
-    case N64_AXISX: return n64.axis_x();
-    case N64_AXISY: return n64.axis_y();
-    default: return 0;
+void updateButton(Button &button, int newValue1, int newValue2) {
+  if (button.value1 != newValue1 || button.value2 != newValue2) {
+    button.value1 = newValue1;
+    button.value2 = newValue2;
+    String str = stringifyButton(button);
+    Serial.println(str);
+    espSerial.println(str);
   }
 }
--- libraries/BetsyShared/BetsyShared.h
+++ libraries/BetsyShared/BetsyShared.h
@@ -1,45 +1,45 @@
 uint8_t betsy_receiverAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-#define N64_START   0
-#define N64_Z       1
-#define N64_B       2
-#define N64_A       3
-#define N64_L       4
-#define N64_R       5
-#define N64_CUP     6
-#define N64_CDOWN   7
-#define N64_CRIGHT  8
-#define N64_CLEFT   9
-#define N64_DUP     10
-#define N64_DDOWN   11
-#define N64_DRIGHT  12
-#define N64_DLEFT   13
-#define N64_AXISX   14
-#define N64_AXISY   15
+#define N64_START     0
+#define N64_Z         1
+#define N64_B         2
+#define N64_A         3
+#define N64_L         4
+#define N64_R         5
+#define N64_CUP       6
+#define N64_CDOWN     7
+#define N64_CRIGHT    8
+#define N64_CLEFT     9
+#define N64_DUP       10
+#define N64_DDOWN     11
+#define N64_DRIGHT    12
+#define N64_DLEFT     13
+#define N64_JOYSTICK  14
 
 #define DELIM   ':'
 
-#define NUM_BUTTONS  16
-
-struct msg {
-  int buttonId;
-  int buttonValue;
+struct Button {
+  int id;
+  int value1;
+  int value2;
 };
 
-String stringifyMsg(int buttonId, int buttonValue) {
-  return String(buttonId) + DELIM + String(buttonValue);
+String stringifyButton(Button button) {
+  return String(button.id) + DELIM +
+         String(button.value1) + DELIM +
+         String(button.value2);
 }
 
-msg parseMsg(String str) {
+Button parseButton(String str) {
   int index = 0;
-  String parts[2] = {"", ""};
+  String parts[3] = {"", "", ""};
   for (int i = 0; i < str.length(); i++) {
     char c = str.charAt(i);
     if (c == DELIM) {
-      index = 1;
+      index++;
     } else {
       parts[index] += c;
     }
   }
-  return {parts[0].toInt(), parts[1].toInt()};
+  return {parts[0].toInt(), parts[1].toInt(), parts[2].toInt()};
 }
